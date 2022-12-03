352e6aa514c0952dce95362aa9358d55a38d0043
Patrik <22777727+fedorish@users.noreply.github.com>
2022-12-03 16:18:32 +0100

    move queryClient outside app
    improvements from https://github.com/TanStack/query/issues/4170\#issuecomment-1336163824

--- App.js
+++ App.js
@@ -8,44 +8,46 @@ import Todos from './src/Todos';
 import { useAppState } from './src/useAppState';
 import { useOnlineManager } from './src/useOnlineManager';
 import { supabase } from './supabase';
+import { useState } from 'react';
 
 function onAppStateChange(status) {
   if (Platform.OS !== 'web') {
     focusManager.setFocused(status === 'active');
   }
 }
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+      retry: 0,
+    },
+    queries: {
+      retry: 2,
+      cacheTime: 1000 * 10,
+    },
+  },
+});
+
+const asyncPersist = createAsyncStoragePersister({
+  storage: AsyncStorage,
+  dehydrateOptions: {
+    dehydrateMutations: true,
+    dehydrateQueries: false,
+  },
+  throttleTime: 1000,
+});
+
+queryClient.setMutationDefaults(['todos'], {
+  mutationFn: async ({ todo }) => {
+    return supabase.from('todos').insert({ todo });
+  },
+});
+
 export default function App() {
   useAppState(onAppStateChange);
   useOnlineManager();
-
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      mutations: {
-        staleTime: Infinity,
-        cacheTime: Infinity,
-        retry: 0,
-      },
-      queries: {
-        retry: 2,
-        cacheTime: 1000 * 10,
-      },
-    },
-  });
-
-  const asyncPersist = createAsyncStoragePersister({
-    storage: AsyncStorage,
-    dehydrateOptions: {
-      dehydrateMutations: true,
-      dehydrateQueries: false,
-    },
-    throttleTime: 1000,
-  });
-
-  queryClient.setMutationDefaults(['todos'], {
-    mutationFn: async ({ todo }) => {
-      return supabase.from('todos').insert({ todo });
-    },
-  });
 
   return (
     <PersistQueryClientProvider
